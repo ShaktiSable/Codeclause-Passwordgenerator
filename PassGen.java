52584ec576d9cfbd0b963da73c2e79783008572c
ShaktiSable <106767425+ShaktiSable@users.noreply.github.com>
2023-04-30 17:07:59 +0530

    Add files via upload

---
+++ PassGen.java
@@ -0,0 +1,14 @@
+//Password Generator code
+
+import java.util.Scanner;
+
+public class PassGen {
+
+    public static final Scanner keyboard = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Generator generator = new Generator(keyboard);
+        generator.mainLoop();
+        keyboard.close();
+    }
+}
